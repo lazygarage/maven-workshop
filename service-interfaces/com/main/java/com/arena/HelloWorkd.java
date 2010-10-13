cebbd5b9564d4cdd6d3a56e8c1497d42fc754f89
Pawan Pangty <pawan.pangty@gmail.com>
2010-10-13 15:36:38 -0400

    ada

---
+++ service-interfaces/com/main/java/com/arena/HelloWorkd.java
@@ -0,0 +1,13 @@
+package com.arena;
+
+public class HelloWorkd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
